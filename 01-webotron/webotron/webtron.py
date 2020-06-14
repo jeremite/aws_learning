14a1da341cda9b63d096c07e46ac906a67c44697
Livy Li <livy@LivydeMacBook.local>
2020-06-14 15:32:46 -0700

    Create and set up bucket

--- 01-webotron/webotron/webtron.py
+++ 01-webotron/webotron/webtron.py
@@ -1,6 +1,7 @@
 import boto3
 import sys
 import click
+from botocore.exceptions import ClientError
 session = boto3.Session(profile_name='travis')
 s3 = session.resource('s3')
 
@@ -22,6 +23,50 @@ def list_bucket_objects(bucket):
 	bucket = s3.Bucket(bucket)
 	for obj in bucket.objects.all():
 		print(obj)
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+	"Create and configure s3 bucket"
+	s3_bucket = None
+
+	try:
+		s3_bucket = s3.create_bucket(Bucket=bucket)
+	except ClientError as e:
+		if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+			s3_bucket=s3.Bucket(bucket)
+		else:
+			raise 3
+
+	policy = """
+	{
+	"Version":"2012-10-17",
+	"Statement":[
+	{
+	  "Sid":"PublicRead",
+	  "Effect":"Allow",
+	  "Principal": "*",
+	  "Action":["s3:GetObject"],
+	  "Resource":["arn:aws:s3:::%s/*"]
+	}
+	]
+	}
+	"""% s3_bucket.name
+	policy = policy.strip()
+	pol = s3_bucket.Policy()
+	pol.put(Policy=policy)
+
+	ws = s3_bucket.Website()
+	ws.put(WebsiteConfiguration={
+	        'ErrorDocument': {
+	            'Key': 'error.html'
+	        },
+	        'IndexDocument': {
+	            'Suffix': 'index.html'
+	        }})
+
+	return
+
 
 if __name__ == '__main__':
 	cli()
